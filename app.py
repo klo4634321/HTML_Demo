96e80b64b4387f6dffd78f4780ec153f8e577fc0
klo4634321 <klo4634321@gmail.com>
2025-04-15 08:26:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -33,4 +34,5 @@ def contact():
     return render_template('contact.html', messages=messages)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
